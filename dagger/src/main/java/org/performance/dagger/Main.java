2f173bc2f6661ddfa8a66f4367eb4c8ed5065aaf
hunteerq <hunteerq@icloud.com>
2020-12-17 11:40:16 +0100

    Fixing dependency management

--- dagger/src/main/java/org/performance/dagger/Main.java
+++ dagger/src/main/java/org/performance/dagger/Main.java
@@ -1,13 +1,10 @@
 package org.performance.dagger;
 
-import client.HttpClientWrapper;
-import client.MockApiService;
-
 public class Main {
 
     public static void main(String[] args) {
         Service service = new Service();
         service.ask();
-
     }
+
 }
